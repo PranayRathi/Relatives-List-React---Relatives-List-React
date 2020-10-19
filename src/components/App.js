565467d94fcc0878fd5c8f16b038fc32b9d884e0
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-19 21:59:29 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  relatives = ["4wall", "roof", "me"];
+  render() {
+    return (
+      <>
+        <ol key="realativeList">
+          {this.relatives.map((relatives, index) => (
+            <li key={`realativeListItem${index + 1}`}>{relatives}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
-
 export default App;
